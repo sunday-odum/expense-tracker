33a4bb72982c43fad4fd1bbe0252adf39cdcfc09
mastertech <sundayodum@gmail.com>
2021-07-14 13:05:56 +0100

    Added component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Header from "./components/Header";
-import './App.css'
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
+
+import './App.css';
 
 function App() {
   return (
     <div>
       <Header />
-    </div>
+      <Balance />
+      <IncomeExpenses />
+      <TransactionList/>
+      <AddTransaction />
+    </div> 
   );
 }
 
